8733a3c7324d9034ce0e4beb6b316addbdfc4164
yyjinlong <914580714@qq.com>
2018-12-18 20:14:52 +0800

    lanus system

--- lanus/bastion/lib/totp.py
+++ lanus/bastion/lib/totp.py
@@ -15,9 +15,7 @@ CONF = cfg.CONF
 
 class TOTPService(object):
 
-    def verify_token(self, token):
-        pass
-
-    def _verify(self, token):
-        # NOTE(联系3次连接OTP服务失败, 则进行降级.)
-        pass
+    def verify(self, token):
+        # NOTE(连续3次连接OTP服务失败, 则进行降级.)
+        for i in range(3):
+            pass
